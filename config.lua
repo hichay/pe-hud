1bbe5ba509a234cfc2badf2c7c59cfd4db73b548
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-04-18 00:24:42 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,7 @@ Config = {}
 -- Needs to be changed to your needs
 Config.MaxPlayers = 32 -- This cannot go under the max level of players
 Config.VoiceChange = 'z'
+Config.openKey = 'f7'
 
 -- Optional (will hide armour if == 0 and oxygen if not in water)
 Config.ShowArmour = true
